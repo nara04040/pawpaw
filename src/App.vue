614937ebf74d1bf7928d95e0c4710e2959afb422
moon <anstnqls554@gmail.com>
2022-09-07 00:07:19 +0900

    feat : 헤더 미완성 / #2

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,15 @@
 <template>
-  <div id="wrap">
-    <nav>
-      <router-link to="/">Home</router-link> | <router-link to="/dogpage">DogPage</router-link> |
-      <router-link to="/catpage">Cat Page</router-link> |
-      <router-link to="/toypage">Toy</router-link> |
-      <router-link to="/qnapage">Q&A</router-link>
-    </nav>
-    <div class="login">
-      <router-link to="/loginpage">Login</router-link>
-    </div>
-    <router-view />
-  </div>
+  <HeaderPart></HeaderPart>
+  <router-view />
+  <FooterPart></FooterPart>
 </template>
 
+<script>
+import HeaderPart from './components/HeaderPart.vue'
+import FooterPart from './components/FooterPart.vue'
+export default {
+  components: { HeaderPart, FooterPart }
+}
+</script>
 <style>
-a {
-  text-decoration: none;
-  color: inherit;
-}
-ul,
-li {
-  list-style: none;
-}
-.clearfix::after {
-  content: '';
-  position: relative;
-  display: block;
-  width: 100%;
-  clear: both;
-}
-.inner {
-  width: 1400px;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  padding: 0 20px;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-.login {
-  display: block;
-  position: absolute;
-  top: 10px;
-  right: 100px;
-}
 </style>
--- src/components/HeaderPart.vue
+++ src/components/HeaderPart.vue
@@ -1,41 +1,81 @@
 <template>
-  <div class="header">
-    <h1>logo</h1>
-    <div id="main-logo">mainlogo</div>
+  <div>
+    <header class="header">
+      <div class="inner">
+        <router-link to="/" class="logo"></router-link>
+      </div>
+      <nav>
+      <router-link to="/">Home</router-link> | <router-link to="/dogpage">DogPage</router-link> |
+      <router-link to="/catpage">Cat Page</router-link> |
+      <router-link to="/toypage">Toy</router-link> |
+      <router-link to="/qnapage">Q&A</router-link>
+    </nav>
+    <div class="login">
+      <router-link to="/loginpage">Login</router-link>
+    </div>
+    </header>
   </div>
 </template>
+
 <script>
 export default {
-  components: {},
-  data() {
-    return {
-      sampleDate: ''
-    }
-  },
-  setup() {},
-  created() {},
-  mounted() {},
-  unmounted() {},
-  methods: {}
+  setup() {}
 }
 </script>
-<style scoped>
-.header {
+
+<style>
+.inner{
+  max-width: 1400px;
   width: 100%;
-  height: 130px;
-  background-color: yellowgreen;
+  margin: 0 auto;
 }
-h1 {
+
+.logo{
+  display: block;
+  width: 100px;
+  height: 100px;
+  background: url('@/assets/images/logo.png') no-repeat center;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+ul,
+li {
+  list-style: none;
+}
+.clearfix::after {
+  content: '';
+  position: relative;
+  display: block;
+  width: 100%;
+  clear: both;
+}
+.inner {
+  width: 1400px;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 20px;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.login {
+  display: block;
   position: absolute;
   top: 10px;
-  left: 30px;
-  background-color: blue;
-}
-#main-logo {
-  display: inline-block;
-  width: 50px;
-  height: 30px;
-  margin: 0 auto;
-  background-color: green;
+  right: 100px;
 }
 </style>
