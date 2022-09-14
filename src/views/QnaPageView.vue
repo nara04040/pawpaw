cb44ce2f48c36eb02df721427ac1c5614b2177c5
yhist <107976094+yhist@users.noreply.github.com>
2022-09-14 22:03:34 +0900

    Feat/#4 (#6)
    * feat : Bootstrap 적용  /#4
    
    * feat : bt 생성

--- src/views/QnaPageView.vue
+++ src/views/QnaPageView.vue
@@ -1,15 +1,54 @@
 <template>
-  <div>
-    <ul class="qna-list">
-      <li>물어보세요</li>
-      <li>물어보세요</li>
-      <li>물어보세요</li>
-      <li>물어보세요</li>
-    </ul>
-    <button>입력</button>
-    <button>입력</button>
-  </div>
+  <section class="qna">
+    <div class="inner">
+      <div class="qna-visual">
+        <p>CUSTOMER CENTER</p>
+        <h2>QnA</h2>
+        <span>
+          PAWPAW에 궁금하신 점이 있으면 <br />
+          바로 문의해주세요
+        </span>
+      </div>
+      <div class="qna-content">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Content</th>
+              <th scope="col">Name</th>
+              <th scope="col">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="qna-bt clearfix">
+          <button class="add-bt">글쓰기</button>
+          <button class="delete-bt">삭제</button>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
 <script>
 export default {
   components: {},
@@ -25,23 +64,40 @@ export default {
   methods: {}
 }
 </script>
+
 <style scoped>
-.qna-list {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 50px;
+.qna .inner {
+  width: 1400px;
+  margin: 0 auto;
 }
-.qna-list > li {
-  width: 200px;
-  height: 100px;
-  margin-bottom: 20px;
-  background-color: skyblue;
+
+.qna-visual {
+  position: relative;
+  display: block;
+  padding: 30px;
+  text-align: left;
+  background-color: rgb(172, 172, 172);
+  border-radius: 10px 10px 0 0;
+  margin: 25px 0;
+  opacity: 0.7;
 }
-button {
-  padding: 20px;
-  margin-right: 40px;
-  margin-left: 40px;
+
+.qna-visual h2 {
+  text-shadow: 0px 1px 4px rgb(119, 119, 119);
+  color: #fff;
+}
+.qna-content {
+  position: relative;
+  display: block;
+}
+
+.qna-bt {
+  position: relative;
+  display: block;
+  float: right;
+  margin-right: 20px;
+}
+
+.add-bt {
 }
 </style>
