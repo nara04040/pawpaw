09fe621f5eee06d5b3c440284e0cefd27e2dff3f
yhist <yhyh0903@gmail.com>
2022-09-14 14:35:05 +0900

    feat : bt 생성

--- src/views/QnaPageView.vue
+++ src/views/QnaPageView.vue
@@ -2,16 +2,21 @@
   <section class="qna">
     <div class="inner">
       <div class="qna-visual">
+        <p>CUSTOMER CENTER</p>
         <h2>QnA</h2>
+        <span>
+          PAWPAW에 궁금하신 점이 있으면 <br />
+          바로 문의해주세요
+        </span>
       </div>
-      <div>
+      <div class="qna-content">
         <table class="table">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">No.</th>
+              <th scope="col">Content</th>
+              <th scope="col">Name</th>
+              <th scope="col">Date</th>
             </tr>
           </thead>
           <tbody>
@@ -29,15 +34,21 @@
             </tr>
             <tr>
               <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
             </tr>
           </tbody>
         </table>
+        <div class="qna-bt clearfix">
+          <button class="add-bt">글쓰기</button>
+          <button class="delete-bt">삭제</button>
+        </div>
       </div>
     </div>
   </section>
 </template>
+
 <script>
 export default {
   components: {},
@@ -64,15 +75,29 @@ export default {
   position: relative;
   display: block;
   padding: 30px;
+  text-align: left;
   background-color: rgb(172, 172, 172);
   border-radius: 10px 10px 0 0;
   margin: 25px 0;
-  opacity: 0.8;
+  opacity: 0.7;
 }
 
 .qna-visual h2 {
-  text-align: left;
   text-shadow: 0px 1px 4px rgb(119, 119, 119);
   color: #fff;
+}
+.qna-content {
+  position: relative;
+  display: block;
+}
+
+.qna-bt {
+  position: relative;
+  display: block;
+  float: right;
+  margin-right: 20px;
+}
+
+.add-bt {
 }
 </style>
