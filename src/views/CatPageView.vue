6f63434426d65f50928ea310d2a8840e53e7af1d
hwangjiwon <107976059+jiwonchu@users.noreply.github.com>
2022-09-14 22:03:08 +0900

    고양이페이지수정/#5 (#10)

--- src/views/CatPageView.vue
+++ src/views/CatPageView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="a">고먐미 고양고양</div>
 
+  <div class="item-tp">
+    <a href="#">전체</a>
+    <a href="#">건식</a>
+    <a href="#">습식</a>
+  </div>
   <div class="item">
     <ul class="item-list clearfix">
       <li>아이템 1</li>
@@ -39,6 +44,26 @@ export default {
 }
 </script>
 <style scoped>
+
+  .item-tp{
+    display: block;
+    width: 150px;
+    height: 150px;
+    margin-left:25px;
+
+  }
+
+  .item-tp > a{
+
+display:inline;
+    width:50px;
+    height: 80px;
+margin-left:10px;
+  }
+  .item-tp>a:hover{
+
+    color:#FC8675;
+  }
 .a {
   margin-top: 80px;
   margin-bottom: 150px;
@@ -49,5 +74,7 @@ export default {
   height: 100px;
   background-color: skyblue;
   margin-right: 50px;
+  background:url('../assets/images/30edd2ad28c0e2372db45c883c0c2848.jpg')no-repeat center;
+  background-size:cover;
 }
 </style>
